80d6cc2d711df3cd52644fa9e73c6f727dc196a7
amexabee <amexabee@yahoo.com>
2023-02-01 12:57:02 +0200

    Modify Person class, create Student class

--- src/person.rb
+++ src/person.rb
@@ -1,12 +1,12 @@
 class Person
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(age, name = "Unknown", parent_permission = true)
       @id = Random.rand(1..1000)
       @name = name
       @age = age
       @parent_permission = parent_permission
   end
 
-  attr_reader :name
+  attr_reader :id
   attr_accessor :name
   attr_accessor :age
 
