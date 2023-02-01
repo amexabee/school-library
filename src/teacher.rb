ca02eee1cbeaf68f845079ae3f3f4e2ebcc3931a
amexabee <amexabee@yahoo.com>
2023-02-01 17:27:56 +0200

    Fix linter

--- src/teacher.rb
+++ src/teacher.rb
@@ -1,13 +1,12 @@
-require "./person.rb"
+require './person'
 
-class Teacher < Person 
-  def initialize(age, specialization, name = "Unknown", parent_permission = true)
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
-  
+
   def can_use_services?
     true
   end
-
 end
