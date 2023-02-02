a52de11226867489429e33fa60278d1354127a60
amexabee <amexabee@yahoo.com>
2023-02-02 19:56:01 +0200

    Add more associations and relations

--- src/person.rb
+++ src/person.rb
@@ -1,16 +1,17 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  attr_reader :id, :visits
+  attr_accessor :name, :age
+  
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @visits = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def correct_name
     @name
--- src/student.rb
+++ src/student.rb
@@ -1,9 +1,15 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  attr_accessor :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+  end
+
+  def classroom=(classroom) 
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
