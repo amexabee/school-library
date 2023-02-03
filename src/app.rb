7609ffd447ee20b99952670cfaaee66979ddd843
amexabee <amexabee@yahoo.com>
2023-02-03 22:23:28 +0200

    Modify the display of list books

--- src/app.rb
+++ src/app.rb
@@ -51,8 +51,7 @@ class App
 
   def list_books
     books.each do |element|
-        puts "Title: #{element.title}"
-        puts "Author: #{element.author}"
+        puts "Title: \"#{element.title}\", Author: #{element.author}"
     end
   end
 
