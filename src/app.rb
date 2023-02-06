3e844bcf5c311f5341817f6e46d485f31a4f37f9
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-06 17:04:19 -0300

    refactor app.rb

--- src/app.rb
+++ src/app.rb
@@ -1,6 +1,7 @@
 require_relative 'book'
-require_relative 'student'
-require_relative 'teacher'
+require_relative './functions/list_books'
+require_relative './functions/list_persons'
+require_relative './functions/create_person'
 
 class App
   attr_reader :books, :persons, :rentals, :prompts
@@ -10,8 +11,8 @@ class App
     @persons = []
     @rentals = []
   end
-  # rubocop:disable Metrics
 
+  # rubocop:disable Metrics
   def receive_input(number)
     case number
     when 1
@@ -39,70 +40,6 @@ class App
     end
   end
   # rubocop:enable Metrics
-
-  def list_books
-    books.each do |element|
-      puts "Title: \"#{element.title}\", Author: #{element.author}"
-    end
-  end
-
-  def list_persons
-    persons.each do |element|
-      if element.is_a?(Student)
-        puts "[Student] Name: #{element.name}, ID: #{element.id}, Age: #{element.age}"
-      else
-        puts "[Teacher] Name: #{element.name}, ID: #{element.id}, Age: #{element.age}"
-      end
-    end
-  end
-
-  def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    input = gets.chomp
-    case input
-    when '1'
-      create_student
-    when '2'
-      create_teacher
-    else
-      puts "\n"
-      puts 'Warning! Please provide a valid number'
-    end
-  end
-
-  def create_student
-    age = prompt_age
-    print 'Name: '
-    name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
-    gets.chomp
-    student = Student.new(age, name)
-    persons << student
-    puts 'Student created successfully'
-  end
-
-  def create_teacher
-    age = prompt_age
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    persons << teacher
-    puts 'Teacher created successfully'
-  end
-
-  def prompt_age
-    print 'Age: '
-    num = gets.chomp
-    age = num.to_i
-    if age.zero?
-      puts "\n"
-      puts 'Warning! Please provide a valid age'
-      prompt_age
-    end
-    age
-  end
 
   def create_book
     print 'Title: '
