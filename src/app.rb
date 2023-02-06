61ad21ca659f2b9624c6e2337ca90c919dca1229
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-06 16:50:28 -0300

    Add selection test to main

--- src/app.rb
+++ src/app.rb
@@ -9,22 +9,10 @@ class App
     @books = []
     @persons = []
     @rentals = []
-    @prompts = ['Please choose an option by entering a number', '1. List all books', '2. List all people',
-                '3. Create a person', '4. Create a book', '5. Create a rental',
-                '6. List all rentals for a given person id', '7. Exit']
   end
-
-  def prompt
-    puts "\n"
-    prompts.each do |element|
-      puts element
-    end
-  end
-
   # rubocop:disable Metrics
-  def receive_input
-    input = gets.chomp
-    number = input.to_i
+
+  def receive_input(number)
     case number
     when 1
       list_books
--- src/main.rb
+++ src/main.rb
@@ -3,11 +3,18 @@ require_relative 'app'
 def main
   puts 'Welcome to School Library App!'
   app = App.new
-
+  options = ['Please choose an option by entering a number', '1. List all books', '2. List all people',
+  '3. Create a person', '4. Create a book', '5. Create a rental',
+  '6. List all rentals for a given person id', '7. Exit']
   repeat = true
   while repeat
-    app.prompt
-    repeat = app.receive_input
+    puts "\n"
+    options.each do |element|
+      puts element
+    end
+    input = gets.chomp
+    number = input.to_i
+    repeat = app.receive_input(number)
   end
 end
 
