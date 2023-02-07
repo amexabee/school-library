dd1eef0d31d7fdda794febfb24518af62c9660d5
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-07 12:20:11 -0300

    Add preserve data to books

--- src/app.rb
+++ src/app.rb
@@ -1,9 +1,12 @@
+require 'json'
+require 'fileutils'
 require_relative './functions/list_books'
 require_relative './functions/list_persons'
 require_relative './functions/create_person'
 require_relative './functions/create_book'
 require_relative './functions/create_rental'
 require_relative './functions/list_rentals'
+require_relative './book'
 
 class App
   attr_reader :books, :persons, :rentals
@@ -33,5 +36,28 @@ class App
       puts 'Warning! Please provide a valid number'
     end
     true
+  end
+
+  def save_data
+    arr = []
+    books.each do |book|
+      obj = {
+        'title' => book.title,
+        'author' => book.author
+      }
+      arr.push(obj)
+    end
+    FileUtils.mkdir_p('data')
+    File.write('./data/books.json', JSON.generate(arr))
+    puts 'Data saved successfully'
+  end
+
+  def load_data
+    puts 'LOADING...'
+    file_data = JSON.parse(File.read('./data/books.json'))
+    file_data.each do |item|
+      book = Book.new(item['title'], item['author'])
+      books << book
+    end
   end
 end
