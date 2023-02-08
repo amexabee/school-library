a1c4c4d2389f3741c49fe245f376c73f20e2c21b
amexabee <amexabee@yahoo.com>
2023-02-08 21:57:11 +0200

    Add testing for Teacher class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,7 +4,7 @@ describe Student do
   before :each do
     @student = Student.new 11, 'Name'
   end
-  describe 'Testing for Person Class' do
+  describe 'Testing for Student Class' do
     it 'Take 3 parameters and return a Person object' do
       expect(@student).to be_instance_of Student
     end
@@ -18,11 +18,6 @@ describe Student do
     end
 
     it 'Check that can\t use service' do
-      expect(@student.can_use_services?).to be true
-    end
-
-    it 'Check of age will be > 18 and can use service' do
-      @student = Student.new 19, 'Name'
       expect(@student.can_use_services?).to be true
     end
 
---
+++ spec/teacher_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../src/teacher'
+
+describe Teacher do
+  before :each do
+    @teacher = Teacher.new 30, 'Chemistry', 'Name'
+  end
+  describe 'Testing for Teacher Class' do
+    it 'Take 3 parameters and return a Person object' do
+      expect(@teacher).to be_instance_of Teacher
+    end
+
+    it 'Return the correct Name' do
+      expect(@teacher.name).to eql 'Name'
+    end
+
+    it 'Return the correct Age' do
+      expect(@teacher.age).to eql 30
+    end
+
+    it 'Return the correct Specialization' do
+        expect(@teacher.specialization).to eql 'Chemistry'
+      end
+
+    it 'Check that can\t use service' do
+      expect(@teacher.can_use_services?).to be true
+    end
+    
+  end
+end
